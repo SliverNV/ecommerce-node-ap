f1c77babb6a19b06b3007d9577c9b3d4081c3e44
SliverNV <sliver.smithbs@gmail.com>
2023-09-26 22:09:25 -0300

    Commit do dia 26-09-2023

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,51 @@
-import { Categoria } from "@modules/catalogo/domain/categoria/categoria.entity";
-import { PrismaClient } from "@prisma/client";
-import { DomainException } from "@shared/domain/domain.exception";
+import { Categoria } from '@modules/catalogo/domain/categoria/categoria.entity';
+import { CategoriaPrismaRepository } from '@modules/catalogo/infra/database/categoria.prisma.repository';
+import { PrismaClient } from '@prisma/client';
+import { DomainException } from '@shared/domain/domain.exception';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log: ['query', 'info'],
+    errorFormat: 'pretty'
+});
 
 async function main() {
+   
+    prisma.$connect().then(
+        async () => {
+            console.log('Postgres Conectado');
+        }
+    );
 
-    ///////////////////
-    //Criar Categoria//
-    ///////////////////
+    const categoriaRepo = new CategoriaPrismaRepository(prisma);
 
-    // let categoria: Categoria;
-    // categoria = Categoria.criar({nome:'mesa'});
+    //const categoriaRecuperada = await categoriaRepo.recuperarPorUuid("c2666bdb-c055-40bb-951b-32f899f41e30");
 
-    // ////////////////////////////////
-    // //Persistir Categoria no Banco//
-    // ////////////////////////////////
+    //console.log(categoriaRecuperada);
 
-    // await prisma.categoria.create({
-    //     data: {
-    //         id: categoria.id,
-    //         nome: categoria.nome
-    //     }
-    // });
+    //const categoria: Categoria = Categoria.criar({
+    //    nome:'Banho'
+    //})    
 
-    ////////////////////////////////
-    //Atualizar Categoria no Banco//
-    ////////////////////////////////
+    //const categoriaInserida = await categoriaRepo.inserir(categoria);
 
-    const categoriaRecuperada = await prisma.categoria.update({
-        where: {id: "c544a6d0-3595-4e88-b181-e56c3c86a7f9" },
-        data: {nome: "banho" },
-    })
+    //console.log(categoriaInserida);
 
+    //const categorias = await categoriaRepo.recuperarTodos();
 
-    /////////////////////
-    //Listar Categorias//
-    /////////////////////
+    //console.log(categorias);
 
-    const ListaCategorias = await prisma.categoria.findMany();
-    console.log(ListaCategorias);
-    
+    //const categoria = Categoria.recuperar({
+    //    id: "c2666bdb-c055-40bb-951b-32f899f41e30",
+    //    nome: "Mesa"
+    //})    
+
+    //const categoriaAtualizada = await categoriaRepo.atualizar(categoria.id,categoria);
+
+    //console.log(categoriaAtualizada)
+
+    const categoriaDeletada = await categoriaRepo.deletar("c544a6d0-3595-4e88-b181-e56c3c86a7f9");
+    console.log(categoriaDeletada)
+
 }
 
 main()
@@ -48,14 +53,14 @@ main()
         await prisma.$disconnect()
     })
     .catch(async (error) => {
-        if (error instanceof DomainException) {
-            console.log('Exceção de Dóminio');
-            console.log(error.message);
-        }
-        else {
-            console.log('Outras Exceções');
-            console.log(error.message);
-        }
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+       if (error instanceof DomainException) {
+           console.log('Execeção de Dóminio');
+           console.log(error.message);
+       }
+       else {
+           console.log('Outras Exceções');
+           console.log(error.message);
+       }
+       await prisma.$disconnect()
+       process.exit(1)
+   })
