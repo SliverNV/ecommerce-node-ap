f461899f9f5a77808acf2759cb1317921a7f513e
SliverNV <sliver.smithbs@gmail.com>
2023-10-17 20:13:51 -0300

    Aplicando Testes de Unidade Automatizados nos Repositórios

--- src/modules/catalogo/domain/produto/produto.entity.ts
+++ src/modules/catalogo/domain/produto/produto.entity.ts
@@ -3,6 +3,7 @@ import { Entity } from "@shared/domain/entity";
 import { Categoria } from "../categoria/categoria.entity";
 import { ProdutoExceptions } from "./produto.exception";
 import { CriarProdutoProps, IProduto, RecuperarProdutoProps, StatusProduto } from "./produto.types";
+import { RecuperarCategoriaProps } from "../categoria/categoria.types";
 
 class Produto extends Entity<IProduto> implements IProduto {
 
@@ -139,7 +140,7 @@ class Produto extends Entity<IProduto> implements IProduto {
         this.nome = produto.nome;
         this.descricao = produto.descricao;
         this.valor = produto.valor;
-        this.categorias = produto.categorias;
+        this.categorias = produto.categorias.map((categoria) => { return Categoria.recuperar(categoria as RecuperarCategoriaProps)});
         this.dataCriacao = produto.dataCriacao;
         this.dataAtualizacao = produto.dataAtualizacao;
         this.dataExclusao = produto.dataExclusao;
