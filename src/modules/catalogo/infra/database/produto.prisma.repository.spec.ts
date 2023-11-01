6c64050623f4475caf462842efba9347ffd762ec
Robert Smith <121267823+SliverNV@users.noreply.github.com>
2023-10-31 21:30:12 -0300

    Refatorando Teste Automatizado de produto.prisma.repository.spec

--- src/modules/catalogo/infra/database/produto.prisma.repository.spec.ts
+++ src/modules/catalogo/infra/database/produto.prisma.repository.spec.ts
@@ -1,88 +1,101 @@
 import { PrismaClient } from "@prisma/client";
-import { DeepMockProxy, mockDeep, mockReset } from "vitest-mock-extended";
+import { afterEach, beforeAll, describe, expect, test, vi } from "vitest";
+import { DeepMockProxy, mockDeep, mockReset } from 'vitest-mock-extended';
 import { ProdutoPrismaRepository } from "./produto.prisma.repository";
-import { afterEach, beforeAll, expect, test, vi, describe } from "vitest";
-import { faker } from "@faker-js/faker";
-import { Produto } from "@modules/catalogo/domain/produto/produto.entity";
-import { ProdutoMap } from "../mappers/produto.map";
 import { StatusProduto } from "@modules/catalogo/domain/produto/produto.types";
+import { Produto } from "@modules/catalogo/domain/produto/produto.entity";
+import { Categoria } from "@modules/catalogo/domain/categoria/categoria.entity";
+import { ProdutoMap } from "../mappers/produto.map";
 import { produtoIncludeCategoriaPrisma } from "@shared/infra/database/prisma.types";
+import { faker } from "@faker-js/faker";
 
 const prismaMock: DeepMockProxy<PrismaClient> = mockDeep<PrismaClient>();
 let produtoRepositorio: ProdutoPrismaRepository;
 let UUIDValido: string;
+let nomeProdutoValido: string;
+let descricaoProdutoValido: string;
+let valorProdutoValido: number;
+let dataCriacaoProduto: Date;
+let dataAtualizacaoProduto: Date;
+let dataExclusaoProduto: Date;
+let statusProduto: StatusProduto;
+let categoriaProduto01: Categoria;
+let categoriaProduto02: Categoria;
 
 describe('Repositório Prisma: Produto', () => {
 
     beforeAll(async () => {
 
+        //Passando o prisma mockado para o repositório
         produtoRepositorio = new ProdutoPrismaRepository(prismaMock);
+
+        //Preencendo as variáveis com dados em conformidade com as restrições da regra de negócio
         UUIDValido = faker.string.uuid(); // Retorna um UUID v4
-        
+        nomeProdutoValido = faker.string.alpha({ length: { min: 5, max: 50 } });
+        descricaoProdutoValido = faker.string.alpha({ length: { min: 10, max: 200 } });
+        valorProdutoValido = faker.number.int({ min: 1, max: 2000 });
+        dataCriacaoProduto = faker.date.anytime();
+        dataAtualizacaoProduto = faker.date.anytime();
+        dataExclusaoProduto = faker.date.anytime();
+        statusProduto = StatusProduto.ATIVO;
+
+        //Criando categorias válidas com dados simulados
+        categoriaProduto01 = Categoria.criar({ nome: faker.string.alpha({ length: { min: 3, max: 50 } }) });
+        categoriaProduto02 = Categoria.criar({ nome: faker.string.alpha({ length: { min: 3, max: 50 } }) });
+
     });
 
     afterEach(() => {
         vi.restoreAllMocks();
-        mockReset(prismaMock);
+        mockReset(prismaMock)
     });
 
     describe('Recuperar Produto por ID', () => {
 
-        test('Deve Recuperar Um Produto por UUID', async () => {
+        test('Deve Recuperar Uma Categoria por UUID', async () => {
 
-            const ProdutoPrisma = {
+            const produtoPrisma = {
                 id: UUIDValido,
-                nome: 'nomeProduto',
-                descricao: 'descricao teste',
-                valor: 0,
-                dataCriacao: faker.date.anytime(),
-                dataAtualizacao: faker.date.anytime(),
-                dataExclusao: faker.date.anytime(),
-                status: StatusProduto.ATIVO,
+                nome: nomeProdutoValido,
+                descricao: descricaoProdutoValido,
+                valor: valorProdutoValido,
+                dataCriacao: dataCriacaoProduto,
+                dataAtualizacao: dataAtualizacaoProduto,
+                dataExclusao: dataExclusaoProduto,
+                status: statusProduto,
                 categorias: [
-                  {
-                    produtoId: '1aa1b385-6d70-486e-830b-4accef6dd144',
-                    categoriaId: '2bfddfb4-6545-4b4b-95f0-daffed38cfe5',
-                    dataCriacao: faker.date.anytime(),
-                    dataAtualizacao: faker.date.anytime(),
-                    categoria: {
-                        id: '2bfddfb4-6545-4b4b-95f0-daffed38cfe5',
-                        nome: 'Cozinha',
-                        dataCriacao: faker.date.anytime(),
-                        dataAtualizacao: faker.date.anytime()
+                    {
+                        produtoId: UUIDValido,
+                        categoriaId: categoriaProduto01.id,
+                        dataCriacao: new Date(),
+                        dataAtualizacao: new Date(),
+                        categoria: {
+                            id: categoriaProduto01.id,
+                            nome: categoriaProduto01.nome,
+                            dataCriacao: new Date(),
+                            dataAtualizacao: new Date()
+                        }
                     }
-                  },
-                  {
-                    produtoId: '1aa1b385-6d70-486e-830b-4accef6dd144',
-                    categoriaId: '0629ee7a-5063-4565-bc97-363248fac22d',
-                    dataCriacao: faker.date.anytime(),
-                    dataAtualizacao: faker.date.anytime(),
-                    categoria: {
-                        id: '0629ee7a-5063-4565-bc97-363248fac22d',
-                        nome: 'nomeCategoria',
-                        dataCriacao: faker.date.anytime(),
-                        dataAtualizacao: faker.date.anytime(),
-                    }
-                  }
                 ]
-              };
+            };
 
-            prismaMock.produto.findUnique.mockResolvedValue(ProdutoPrisma);
+            prismaMock.produto.findUnique.mockResolvedValue(produtoPrisma);
 
-            const produto: Produto = ProdutoMap.fromPrismaModelToDomain(ProdutoPrisma);
+            const produto: Produto = ProdutoMap.fromPrismaModelToDomain(produtoPrisma);
 
             const produtoRecuperado = await produtoRepositorio.recuperarPorUuid(produto.id);
 
-            expect(produtoRecuperado).toEqual(produto);
+            expect(produtoRecuperado).toStrictEqual(produto)
             expect(prismaMock.produto.findUnique).toHaveBeenCalledTimes(1);
             expect(prismaMock.produto.findUnique).toBeCalledWith({
                 where: {
                     id: produto.id,
                 },
                 include: produtoIncludeCategoriaPrisma
-            });    
-            }); 
+            });
 
         });
 
     });
+
+});
