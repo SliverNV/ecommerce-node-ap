a2c06df38b8fc754cdd2889e205c5fa586d3b5fc
Robert Smith <121267823+SliverNV@users.noreply.github.com>
2023-11-28 20:49:02 -0300

    Commit da aula do dia 28/11/2023

---
+++ src/modules/catalogo/apresentation/http/rest/categoria.routes.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { recuperarCategoriaPorIdController } from './controllers';
+
+
+const categoriaRouter = express.Router();
+
+categoriaRouter.get(
+    '/:id',
+    (request, response, next) =>  recuperarCategoriaPorIdController.recuperar(request, response, next)
+);
+
+
+export { categoriaRouter };
