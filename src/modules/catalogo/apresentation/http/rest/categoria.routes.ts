20f5830ef31b2364b4ead57587bba2158f2b5a79
Robert Smith <121267823+SliverNV@users.noreply.github.com>
2023-11-28 21:10:24 -0300

    Correção e Outros;

--- src/modules/catalogo/apresentation/http/rest/categoria.routes.ts
+++ src/modules/catalogo/apresentation/http/rest/categoria.routes.ts
@@ -1,6 +1,5 @@
 import express from 'express';
-import { recuperarCategoriaPorIdController } from './controllers';
-
+import { inserirCategoriaController, recuperarCategoriaPorIdController, recuperarTodasCategoriasController } from './controllers';
 
 const categoriaRouter = express.Router();
 
@@ -9,5 +8,14 @@ categoriaRouter.get(
     (request, response, next) =>  recuperarCategoriaPorIdController.recuperar(request, response, next)
 );
 
+categoriaRouter.get(
+    '/',
+    (request, response, next) =>  recuperarTodasCategoriasController.recuperar(request, response, next)
+);
+
+categoriaRouter.post(
+    '/',
+    (request, response, next) =>  inserirCategoriaController.inserir(request, response, next)
+);
 
 export { categoriaRouter };
--- src/modules/catalogo/apresentation/http/rest/controllers/index.ts
+++ src/modules/catalogo/apresentation/http/rest/controllers/index.ts
@@ -1,11 +1,14 @@
-import { recuperarCategoriaPorIdUseCase, recuperarTodasCategoriasUseCase } from "@modules/catalogo/application/use-case";
+import { inserirCategoriaUseCase, recuperarCategoriaPorIdUseCase, recuperarTodasCategoriasUseCase } from "@modules/catalogo/application/use-cases";
 import { RecuperarCategoriaPorIdExpressController } from "./recuperar-categoria-por-id.express.controller";
 import { RecuperarTodasCategoriaExpressController } from "./recuperar-todas-categorias.express.controller";
+import { InserirCategoriaExpressController } from "./inserir-categoria.express.controller";
 
 const recuperarCategoriaPorIdController = new RecuperarCategoriaPorIdExpressController(recuperarCategoriaPorIdUseCase);
 const recuperarTodasCategoriasController = new RecuperarTodasCategoriaExpressController(recuperarTodasCategoriasUseCase);
+const inserirCategoriaController = new InserirCategoriaExpressController(inserirCategoriaUseCase);
 
 export {
     recuperarCategoriaPorIdController,
-    recuperarTodasCategoriasController
+    recuperarTodasCategoriasController,
+    inserirCategoriaController
 }
