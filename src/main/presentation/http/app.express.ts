5afec4b787e42bea1d1d886c2239d2ee14618c4d
Robert Smith <robertsmith.rk1998@gmail.com>
2024-01-11 14:03:43 -0300

    Refactoring: Main File

--- src/main/presentation/http/app.express.ts
+++ src/main/presentation/http/app.express.ts
@@ -1,11 +1,13 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application } from "express";
 import { apiv1Router } from "./rest/api.v1";
 import helmet from "helmet";
 import compression from 'compression';
 import { customMorganMiddleware } from "./middlewares/custom-morgan.middleware";
 import { errorLogger } from "./middlewares/error-logger.middleware";
 import { errorResponder } from "./middlewares/error-responser.middleware";
+import { invalidPath } from "./middlewares/invalid-path.middleware";
 import cors from "cors";
+
 const createExpressApplication = async (): Promise<Application>  => {
     const app: Application = express();
     app.disable('x-powered-by');
@@ -28,7 +30,8 @@ const createExpressApplication = async (): Promise<Application>  => {
     //Middlewares de Rotas
     app.use('/api/v1', apiv1Router);
 
-     //Middleware de Tratamento de Erros (Error Handling)
+    //Middleware de Tratamento de Erros (Error Handling)
+    app.use(invalidPath);
     app.use(errorLogger)
     app.use(errorResponder);
 
